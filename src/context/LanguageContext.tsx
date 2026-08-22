39992de2917ae71cc70d3fe205bb21aa85505627
Erofay <ermiyastesfaye718@gmail.com>
2026-08-22 14:54:59 +0300

    Add translations for multiple languages

---
+++ src/context/LanguageContext.tsx
@@ -0,0 +1,23 @@
+const translations = {
+  en: {
+    gis_map_view: "GIS Map View",
+    incident_log: "Incident Log",
+    sign_in: "Sign In",
+    public: "Public",
+    private: "Private"
+  },
+  am: {
+    gis_map_view: "የGIS ካርታ እይታ",
+    incident_log: "የክስተቶች መዝገብ",
+    sign_in: "ይግቡ",
+    public: "የሕዝብ",
+    private: "የግል"
+  },
+  om: {
+    gis_map_view: "Mullata Kaarta GIS",
+    incident_log: "Galmee Taateewani",
+    sign_in: "Seenaa",
+    public: "Uummata",
+    private: "Dhuunfaa"
+  }
+};
